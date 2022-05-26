594c667786e99bf51ba759b829dbc9ca81634da2
Yash Jain <jyash237@gmail.com>
2022-05-26 13:21:48 +0200

    Changed requiremnts file

--- App/streamlit.py
+++ App/streamlit.py
@@ -1,41 +1,39 @@
 import requests
 import streamlit as st
 
+
 def FileTypeEntry():
     st.header("Loan Prediction")
-    file=st.file_uploader("upload file", type=["csv"])
-    show_file=st.empty()
+    file = st.file_uploader("upload file", type=["csv"])
+    show_file = st.empty()
     if not file:
         show_file.info("Please upload a file".format(''.join(["csv"])))
         return
-    filepath= '../data/'+file.name
+    filepath = '../data/' + file.name
     if st.button("Predict"):
         postUrl = "http://127.0.0.1:8000/fileType?myfiles=" + filepath
         response = requests.post(postUrl)
-        prediction=response.text
-        prediction = prediction.replace('"','')
+        prediction = response.text
+        prediction = prediction.replace('"', '')
         listRes = list(prediction.split(" "))
-        pred=[]
+        pred = []
         for i in listRes:
-            if i =='Y':
+            if i == 'N':
                 pred.append('Yes')
             else:
                 pred.append('No')
         st.dataframe(pred)
+
+
 def Indiviual_entry():
-    product_list=[]
-    product_list.append(st.text_input("Loan_ID"))
-    product_list.append(st.selectbox("Gender",["Male","Female"]))
-    product_list.append(st.selectbox("Married", ["Yes", "No"]))
-    product_list.append(st.selectbox("Dependents", ["0", "1","2"]))
-    product_list.append(st.selectbox("Education",["Graduate","Non-Graduate"]))
-    product_list.append(st.selectbox("Self_Employed",["Yes","No"]))
-    product_list.append(st.text_input("Applicant Income"))
-    product_list.append(st.text_input("CoApplicant Income"))
-    product_list.append(st.text_input("Loan Amount"))
-    product_list.append(st.text_input("Loan Amount Term"))
-    product_list.append(st.selectbox("Credit_History", ["1", "0"]))
-    product_list.append(st.selectbox("Property_Area",["Urban","Semi-Urban","Rural"]))
+    product_list = [st.text_input("Loan_ID"), st.selectbox("Gender", ["Male", "Female"]),
+                    st.selectbox("Married", ["Yes", "No"]), st.selectbox("Dependents", ["0", "1", "2"]),
+                    st.selectbox("Education", ["Graduate", "Non-Graduate"]),
+                    st.selectbox("Self_Employed", ["Yes", "No"]), st.text_input("Applicant Income"),
+                    st.text_input("CoApplicant Income"), st.text_input("Loan Amount"),
+                    st.text_input("Loan Amount Term"),
+                    st.selectbox("Credit_History", ["1", "0"]),
+                    st.selectbox("Property_Area", ["Urban", "Semi-Urban", "Rural"])]
     data = {
         'Loan_ID': product_list[0],
         'Gender': product_list[1],
@@ -52,15 +50,18 @@ def Indiviual_entry():
     }
     if st.button("Predict"):
         postUrl = "http://127.0.0.1:8000/individualEntry"
-        response = requests.post(postUrl,json=data)
+        response = requests.post(postUrl, json=data)
         prediction = response.text
-        if prediction == "Y":
+        print(prediction)
+        if prediction == "N":
             st.success(f"He will return the loan")
         else:
             st.error(f"Sorry!! Customer will not return the loan")
-if __name__=="__main__":
-    Select_type_entry=st.sidebar.selectbox("Select the type of input",["File Type","Indiviual Entry"])
-    if(Select_type_entry=="File Type"):
+
+
+if __name__ == "__main__":
+    Select_type_entry = st.sidebar.selectbox("Select the type of input", ["On Demand Prediction", "File Type"])
+    if Select_type_entry == "File Type":
         FileTypeEntry()
     else:
         Indiviual_entry()
